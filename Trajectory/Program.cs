b00eacdf87b444935ba1709575b68db278803aaa
●Provoke~ ~ZioT● <jstew100@yahoo.com>
2023-05-20 13:48:23 -0400

    Add project files.

---
+++ Trajectory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+using Trajectory;
+
+namespace trajectory
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Create and run the main form of your application
+            Application.Run(new Form_MainMenu());
+        }
+    }
+}
